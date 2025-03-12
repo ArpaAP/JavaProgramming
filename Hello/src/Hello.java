33cf64783d140bcec829844195174ceb319f3ab4
알파 <arpa1705@gmail.com>
2025-03-12 09:22:51 +0900

    update

---
+++ Hello/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    String name;
+
+    public Hello() {
+        name = "World";
+    }
+
+    public void printName() {
+        System.out.println("Hello " + name);
+    }
+}
